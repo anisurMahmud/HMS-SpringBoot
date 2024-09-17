ef9f66c6cde667728cecf9feadd27dac793cd991
anisurMahmud <19202103107@cse.bubt.edu.bd>
2024-09-17 12:11:12 +0600

    found the problem and fixed it, package directory problem

--- src/main/java/com/hms/controller/UserController.java
+++ src/main/java/com/hms/controller/UserController.java
@@ -7,24 +7,25 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller //class is now controller
-@RequestMapping(path = "/user") //url start with /user
+@RequestMapping(path="/user") //url start with /user
+
 public class UserController {
-//    @Autowired        //autowired creates problems it seems. must learn more
-//
-//    private UserRepository userRepository;
-//
-//    @PostMapping(path="/add") // Map ONLY POST Requests
-//    public @ResponseBody String addNewUser(@RequestParam String name, @RequestParam short age){
-//        User newUser = new User();
-//        newUser.setName(name);
-//        newUser.setAge(age);
-//        userRepository.save(newUser);
-//        return "Saved";
-//    }
-//
-//    @GetMapping(path = "/all")
-//    public @ResponseBody Iterable<User> getAllUsers(){
-//        //supposed to return json or xml with the users
-//        return userRepository.findAll();
-//    }
+    @Autowired        //Makes beans, must learn more
+
+    private UserRepository userRepository;
+
+    @PostMapping(path="/add") // Map ONLY POST Requests
+    public @ResponseBody String addNewUser(@RequestParam String name, @RequestParam short age){
+        User newUser = new User();
+        newUser.setName(name);
+        newUser.setAge(age);
+        userRepository.save(newUser);
+        return "Saved";
+    }
+
+    @GetMapping(path = "/all")
+    public @ResponseBody Iterable<User> getAllUsers(){
+        //supposed to return json or xml with the users
+        return userRepository.findAll();
+    }
 }
